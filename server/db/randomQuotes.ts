77e93c8386e127b122822b3a5c6c5d13f80d42dc
Joy Gamayon <joygamz95@gmail.com>
2024-10-07 00:08:10 +1300

    Added database functions

--- server/db/randomQuotes.ts
+++ server/db/randomQuotes.ts
@@ -1,6 +1,35 @@
 import connection from './connection.ts'
-import { Fruit } from '../../models/quote.ts'
+import { RandomQuotes } from '../../models/quote.ts'
 
-export async function getAllFruits(db = connection): Promise<Fruit[]> {
-  return db('fruit').select()
+const db = connection
+
+export async function getAllRandomQuotes(): Promise<RandomQuotes[]> {
+  return db('quotes').select()
+}
+
+// Fetch data by ID
+export async function getRandomQuotesById(id: number): Promise<RandomQuotes | undefined> {
+  return db('quotes').where({ id }).select().first()   
+}
+
+// Add a new task
+export async function addQuote( quote: string, author: string ): Promise<number[]> {
+  return db('quotes').insert({ quote, author })
+}
+
+
+// Update an existing task
+export async function updateTask(
+  id: number, 
+  quote: string, 
+  author: string 
+  ): Promise<number> {
+  return db('quotes') .where('id', id).update({ id, quote, author })
+}
+
+// Delete a task
+export async function deleteTask(id: number): Promise<number> {
+  return db('quotes')
+    .where({ id })
+    .del()
 }
--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,22 @@
 import express from 'express'
 import * as Path from 'node:path'
 
-import fruitRoutes from './routes/fruits.ts'
+import RandomQuoteRoutes from './routes/fruits.ts'
 
 const server = express()
 
+// Middleware to parse JSON bodies
 server.use(express.json())
 
-server.use('/api/v1/quotes', fruitRoutes)
+// Set up the routes for quotes
+server.use('/api/v1/quotes', RandomQuoteRoutes)
 
+// Serve static files in production
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
   server.use('/assets', express.static(Path.resolve('./dist/assets')))
+ 
+  // Catch all route to serve index.html for client-side routing
   server.get('*', (req, res) => {
     res.sendFile(Path.resolve('./dist/index.html'))
   })
